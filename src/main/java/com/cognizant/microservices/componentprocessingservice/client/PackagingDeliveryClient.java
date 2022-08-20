cda8cd6c452fe5fe8f1f9bd34eea9a30b2e1c3b6
Madhu Pranesh <841223@cognizant.com>
2022-08-20 20:16:58 +0530

    Adding feign configs for p&d for aws

--- src/main/java/com/cognizant/microservices/componentprocessingservice/client/PackagingDeliveryClient.java
+++ src/main/java/com/cognizant/microservices/componentprocessingservice/client/PackagingDeliveryClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name="packaging-delivery-service")
+@FeignClient(name="packaging-delivery-service",url = "${PACKAGEDELIVERYURL:http://localhost:8083}")
 public interface PackagingDeliveryClient {
     @GetMapping("/packagingAndDeliveryService/{componentType}/{count}")
     public int packagingAndDeliveryCost(@RequestHeader(name = "Authorization") String token, @PathVariable("componentType") String componentType, @PathVariable("count") int noOfComponents);
