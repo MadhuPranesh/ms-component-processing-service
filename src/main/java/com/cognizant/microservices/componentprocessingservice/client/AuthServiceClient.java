ab536b9b0725079d09ed0e21ffa974813a0d1432
Madhu Pranesh <841223@cognizant.com>
2022-08-20 15:19:45 +0530

    Adding feign configs for auth for aws

--- src/main/java/com/cognizant/microservices/componentprocessingservice/client/AuthServiceClient.java
+++ src/main/java/com/cognizant/microservices/componentprocessingservice/client/AuthServiceClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name="auth-service")
+@FeignClient(name="auth-service",url = "${AUTHSERVICEURL:http://localhost:8081}")
 public interface AuthServiceClient {
 
     @GetMapping("/auth/validateToken")
